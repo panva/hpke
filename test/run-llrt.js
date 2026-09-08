91c921ced98006fcef305de7a1f6459789694898
Filip Skokan <panva.ip@gmail.com>
2026-09-08 16:17:15 +0200

    test: update llrt expectations

--- test/run-llrt.js
+++ test/run-llrt.js
@@ -1,7 +1,8 @@
 import { runRuntimeTests } from './run-runtime.js'
+import { getUnsupportedAlgorithms } from './run.js'
 
 try {
-  await runRuntimeTests({ unsupported: { kem: [], kdf: [], aead: [] } })
+  await runRuntimeTests({ unsupported: getUnsupportedAlgorithms() })
 } catch (error) {
   console.error(error)
   process.exitCode = 1
